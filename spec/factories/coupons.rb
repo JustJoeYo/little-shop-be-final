cc34243898a3ae1c13bdee8e5fab8f832374a2c7
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-04-25 09:00:31 -0700

    coupon model tests

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :coupon do
-    name { "MyString" }
-    code { "MyString" }
-    discount_type { "MyString" }
-    discount_amount { "9.99" }
-    status { "MyString" }
-    merchant { nil }
+    name { Faker::Commerce.promotion_code }
+    code { Faker::Alphanumeric.unique.alphanumeric(number: 8).upcase }
+    discount_type { ['percent', 'dollar'].sample }
+    discount_amount { discount_type == 'percent' ? rand(5..50) : rand(1..20) }
+    status { 'active' }
+    merchant
   end
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Coupon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :merchant }
+    it { should have_many :invoices }
+  end
+
+  describe 'validations' do
+    let(:merchant) { create(:merchant) }
+    
+    subject { 
+      described_class.new(
+        name: 'Test Coupon',
+        code: 'TEST101',
+        discount_type: 'percent', 
+        discount_amount: 10,
+        merchant: merchant
+      ) 
+    }
+    
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :code }
+    it { should validate_uniqueness_of :code }
+    it { should validate_presence_of :discount_type }
+    it { should validate_presence_of :discount_amount }
+    it { should validate_inclusion_of(:discount_type).in_array(%w(percent dollar)) }
+    it { should validate_numericality_of(:discount_amount).is_greater_than(0) }
+    it { should validate_inclusion_of(:status).in_array(%w(active inactive)) }
+  end
+
+  describe 'instance methods' do
+    it '# of invoices using the coupon' do
+      merchant = create(:merchant)
+      coupon = create(:coupon, merchant: merchant)
+      customer = create(:customer)
+      
+      create_list(:invoice, 3, merchant: merchant, customer: customer, coupon: coupon)
+      
+      expect(coupon.used_count).to eq(3)
+    end
+  end
 end
